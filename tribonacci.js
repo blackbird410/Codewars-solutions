f24440ea5977bff48e0426a9473994a35f7d6be0
blackbird410 <ntsrigaud14@gmail.com>
2023-05-30 17:35:30 +0800

    Used the ternary operator for evaluating n

--- tribonacci.js
+++ tribonacci.js
@@ -1,40 +1,18 @@
 function tribonacci(signature, n) {
-  if (n <= 3) {
-    switch(n) {
-      case 0:
-        return [];
-      case 1:
-        return [1];
-      case 2:
-        return [signature[0], signature[1]];
-      case 3:
-        return signature;
-      default:
-        return [];
-        break;
-    }
+  /**
+   * This function determine an array n of the tribonacci sequence. In a tribonacci 
+   * sequence each number is equal to the sum of the three precedent numbers for n > 3.
+   */
 
-  } else {
-    let a = tribonacci(signature, n-1);
-    let j = a.length;
-    signature.push(a[j-3] + a[j-2] + a[j-1]);
-    return signature;
+  if (n <= 3) {
+    return (n === 0) ? []
+    : (n === 1) ? [1]
+    : (n === 2) ? [signature[0], signature[1]]
+    : signature;
   }
   
+  let a = tribonacci(signature, n-1);
+  let j = a.length;
+  signature.push(a[j-3] + a[j-2] + a[j-1]);
+  return signature;  
 }
-
-let a = tribonacci([1,1,1], 10);
-let b = [1,1,1,3,5,9,17,31,57,105];
-console.log(a == b);
-
-
-/*
-doTest([ [1,1,1], 10], [1,1,1,3,5,9,17,31,57,105]   )
-doTest([ [0,0,1], 10], [0,0,1,1,2,4,7,13,24,44]     )
-doTest([ [0,1,1], 10], [0,1,1,2,4,7,13,24,44,81]    )
-doTest([ [1,0,0], 10], [1,0,0,1,1,2,4,7,13,24]      )
-doTest([ [0,0,0], 10], [0,0,0,0,0,0,0,0,0,0]        )
-doTest([ [1,2,3], 10], [1,2,3,6,11,20,37,68,125,230])
-doTest([ [3,2,1], 10], [3,2,1,6,9,16,31,56,103,190] )
-doTest([ [1,1,1],  1], [1])
-*/
